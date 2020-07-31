34e4815cc1cb4b6785e1807eb6a0bb6d6cacf289
Ian Holladay <holladayian@gmail.com>
2020-07-30 21:26:38 -0600

    build structure of game class

---
+++ game.js
@@ -0,0 +1,30 @@
+class Game {
+  constructor() {
+    this.player1 = new Player();
+    this.player2 = new Player();
+  }
+
+  gameBoardLogic() {
+
+  }
+
+  whosTurn() {
+
+  }
+
+  whoWon() {
+
+  }
+
+  isItADraw() {
+
+  }
+
+  saveWinningBoard() {
+
+  }
+
+  resetBoard() {
+
+  }
+}
