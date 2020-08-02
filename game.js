2972d621e14f43e28f2dddb4aa0692c4764fd1d9
Ian Holladay <holladayian@gmail.com>
2020-08-02 17:46:23 -0600

    change who's turn it is to their respective token

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ class Game {
     this.board = ["top-left", "top-center", "top-right", "mid-left", "mid-center", "mid-right", "bottom-left", "bottom-center", "bottom-right"];
     this.player1 = new Player(1, "🤡");
     this.player2 = new Player(2, "🔪");
-    this.turn = true;
+    this.turn;
     this.plays = 0;
     this.winningBoards = [
       [0, 1, 2],
@@ -21,7 +21,6 @@ class Game {
     for (var i = 0; i < this.board.length; i++) {
       if (checkClickLocation.contains(this.board[i])) {
         this.whosTurn();
-        // maybe swith true/false with this.player1.token/this.player2.token
         this.board[i] = this.turn;
         this.checkWin();
         this.checkDraw()
@@ -31,10 +30,10 @@ class Game {
   }
 
   whosTurn() {
-    if (this.turn) {
-      this.turn = false;
+    if (this.turn === this.player2.token) {
+      this.turn = this.player1.token;
     } else {
-      this.turn = true;
+      this.turn = this.player2.token;
     }
   }
 
